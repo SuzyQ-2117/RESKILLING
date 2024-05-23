eae24800b16b10d65a95cc2a9f9f2872a5571f8a
SuzyQ-2117 <153325350+SuzyQ-2117@users.noreply.github.com>
2024-05-23 14:39:09 +0100

    plant website app created, file structure added

---
+++ Day18_React_Thursday/plant-website/src/components/HomeContent.jsx
@@ -0,0 +1,12 @@
+export default function HomeContent() {
+    return (
+        <div className="flex">
+            <div className="divleft">
+                <h1>Bring your home to life.</h1>
+                <h4>Elevate your living space with a touch of nature.</h4>
+                <h4>We offer a range of easy-care houseplants.</h4>
+                <button>Browse plants</button>
+            </div>
+        </div>
+    )
+}
---
+++ Day18_React_Thursday/plant-website/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+export default function HomeContent() {
+    return (
+        <div>
+            <h2>seed theory</h2>
+            <ul>
+                <li>Home</li>
+                <li>Shop</li>
+            </ul>
+        </div>
+    );
+}
