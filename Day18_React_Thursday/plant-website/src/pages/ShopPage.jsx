74fef8ca40ef654e1e1ac2880642bb78e5567a52
SuzyQ-2117 <153325350+SuzyQ-2117@users.noreply.github.com>
2024-05-23 17:03:18 +0100

    final commit - updated flex rules

--- Day18_React_Thursday/plant-website/src/pages/ShopPage.jsx
+++ Day18_React_Thursday/plant-website/src/pages/ShopPage.jsx
@@ -2,16 +2,20 @@ import PlantData from '../plantData.json';
 
 const ShopPage = () => {
     return (
-        <div className="flex card">
+        <div className="center maxwidth">
+            <br/>
             <h1 class="green-text">Our plants</h1>
-            {PlantData.map((Plant)=> (
-                <figure>
-                    <img src={Plant.imageUrl} alt="" />
-                    <h3 class="green-text">{Plant.name}</h3>
-                    <h5 class="green-text">£{Plant.price.toFixed(2)}</h5>
-                    <input className="button" type="button" value="Add to cart"></input>
-                </figure>
-            ))}
+            <br/>
+            <div className="flex card">
+                {PlantData.map((Plant)=> (
+                    <div className="plant">
+                        <img src={Plant.imageUrl} alt="" />
+                        <h3 class="green-text">{Plant.name}</h3>
+                        <h5 class="green-text">£{Plant.price.toFixed(2)}</h5>
+                        <input className="button2" type="button" value="Add to cart"></input>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
